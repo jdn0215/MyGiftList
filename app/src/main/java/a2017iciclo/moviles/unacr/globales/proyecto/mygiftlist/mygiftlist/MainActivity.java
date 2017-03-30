532f4b50ef91184bf205b381b8b998b18dd873c7
jdn0215 <jdn0213@gmail.com>
2017-03-29 20:46:50 -0600

    first

---
+++ app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/MainActivity.java
@@ -0,0 +1,13 @@
+package a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

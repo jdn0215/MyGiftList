ab987bbd1acef4c5018647a7fb0124cfa109c7b2
jdn0215 <jdn0213@gmail.com>
2017-04-02 12:54:47 -0600

    class_tableCreator

---
+++ proyecto/app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/TableCreator.java
@@ -0,0 +1,34 @@
+package a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist;
+
+import android.content.Context;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+/**
+ * Created by jdani on 2/4/2017.
+ */
+
+public class TableCreator {
+    TableLayout table;
+    int maxSize;
+    Context context;
+    public TableCreator(TableLayout tl,Context c,int ms){
+        this.table = tl;
+        this.context = c;
+        this.maxSize = ms;
+    }
+    TableRow getCurrentRow(){
+        TableRow row = (TableRow)table.getChildAt( table.getChildCount() - 1);
+        int  k = row.getChildCount();
+        if( k >= this.maxSize){
+            table.addView(new TableRow(this.context));
+            row = (TableRow)table.getChildAt( table.getChildCount() - 1);
+        }
+        return row;
+    }
+    public void addComponent(Component comp){
+        if(comp!=null){
+            this.getCurrentRow().addView(comp.get());
+        }
+    }
+}

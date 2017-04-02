a93b9e9a681c2b5034c3aa104ff77a8dcba7d2ec
luisrodriguezchaves1 <luigi05-3@hotmail.com>
2017-04-02 16:41:31 -0600

    pequeño cambio en estandarizacion, agregacion de table a mainactivity

--- proyecto/app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/MainActivity.java
+++ proyecto/app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/MainActivity.java
@@ -2,13 +2,23 @@ package a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TableLayout;
+
+import static a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.R.drawable.carpeta;
 
 public class MainActivity extends AppCompatActivity {
-    Folder folders;
+    Folder v_F_M_folders;
+    TableCreator v_TC_M_creator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        folders = new Folder(super.getApplicationContext());
+        v_F_M_folders = new Folder(super.getApplicationContext());
+        TableLayout v_F_M_tb= (TableLayout) findViewById(R.id.carpetas);
+        v_TC_M_creator=new TableCreator(v_F_M_tb,super.getApplicationContext(),3);
+        for(int i=0;i<v_F_M_folders.size();i++){
+            Component comp=new Component(super.getApplicationContext(),v_F_M_folders.get(i),"carpeta.png");
+            v_TC_M_creator.addComponent(comp);
+        }
     }
 }

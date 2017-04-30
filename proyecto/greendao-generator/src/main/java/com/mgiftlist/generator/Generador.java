3a9e38b478e9574d674d1d8ba8b3cbdcbed3f01b
luisrodriguezchaves1 <luigi05-3@hotmail.com>
2017-04-29 23:56:45 -0600

    despues de 1000 intentos y 3 borrones pude generar las clases necesarias para la base de datos

---
+++ proyecto/greendao-generator/src/main/java/com/mgiftlist/generator/Generador.java
@@ -0,0 +1,46 @@
+package com.mgiftlist.generator;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+/**
+ * Created by Luis on 29/4/2017.
+ */
+
+public class Generador {
+
+    private static final String dir="a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db";
+    private static final String dir2="a2017iciclo\\moviles\\unacr\\globales\\proyecto\\mygiftlist\\mygiftlist";
+    Schema schema = new Schema(1,dir);
+
+    private static final String PROJECT_DIR = System.getProperty("user.dir");
+
+    public static void main(String[] args) {
+        Schema schema = new Schema(1, "db");
+        schema.enableKeepSectionsByDefault();
+
+        agregarTablaGifs(schema);
+
+        try {
+            new DaoGenerator().generateAll(schema, PROJECT_DIR+"\\app\\src\\main\\java\\"+dir2+"\\Greendao\\");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void agregarTablaGifs(final Schema schema){
+        Entity gift=addGift(schema);
+    }
+
+    private static Entity addGift(final Schema schema){
+        Entity gift = schema.addEntity("GiftDB");
+        gift.addIdProperty().primaryKey().autoincrement();
+        gift.addStringProperty("img");
+        gift.addStringProperty("descp");
+        gift.addStringProperty("nombre");
+        gift.addStringProperty("folder");
+        gift.addIntProperty("precio");
+        return gift;
+    }
+}

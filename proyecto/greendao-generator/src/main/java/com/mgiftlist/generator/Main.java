9d8ae9e85f5634afe916b71cc11142ce0fa48936
luisrodriguezchaves1 <luigi05-3@hotmail.com>
2017-04-29 20:56:47 -0600

    cambios nuevos para agregar greendao

--- proyecto/greendao-generator/src/main/java/com/mgiftlist/generator/Main.java
+++ proyecto/greendao-generator/src/main/java/com/mgiftlist/generator/Main.java
@@ -1,4 +1,10 @@
 package com.mgiftlist.generator;
 
+
+import de.greenrobot.daogenerator.Schema;
+
 public class Main {
+    public static void main(String[] args){
+        Schema schema = new Schema(1, "com.abc.greendaoexample.db");
+    }
 }

60cfe1e0e2654a26a38f983fb06f5af89bf87f88
luisrodriguezchaves1 <luigi05-3@hotmail.com>
2017-05-26 13:00:29 -0600

    cambios al listview en imagenesvista, aun no carga ya ahorita empieza inge

--- proyecto/app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/ImagenesVista2.java
+++ proyecto/app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/ImagenesVista2.java
@@ -11,16 +11,24 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
 
 import a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db.DaoMaster;
 import a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db.DaoSession;
+import a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db.GiftDB;
 import a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db.GiftDBDao;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+
+import static android.media.CamcorderProfile.get;
 
 
 public class ImagenesVista2 extends BaseDatos{
@@ -32,8 +40,11 @@ public class ImagenesVista2 extends BaseDatos{
         setContentView(R.layout.activity_imagenes_vista2);
         folderactual=super.getIntent().getStringExtra("folderactual");
 
-        cargarGifts();
+        //esta linea debo eliminarla
+        super.saveToSQL(new GiftDB((long)super.sizeLista(),""+R.drawable.vader,"figurilla muy repro de vader :D","Darth Vader",folderactual,999991,0.0,0.0));
 
+
+        cargarGifts();
     }
 
     @Override
@@ -62,4 +73,40 @@ public class ImagenesVista2 extends BaseDatos{
                 return super.onOptionsItemSelected(item);
         }
     }
+    private void populateListView() {
+        ArrayAdapter<Gift> adapter = new MyListAdapter();
+        ListView list = (ListView) findViewById(R.id.listview);
+        list.setAdapter(adapter);
+    }
+
+
+    private class MyListAdapter extends ArrayAdapter<Gift> {
+        public MyListAdapter() {
+            super(ImagenesVista2.this, R.layout.giftcargado, gifts);
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup
+                parent) {
+        // Make sure we have a view to work with (may have been given null)
+            View giftcargado = convertView;
+            if (giftcargado == null) {
+                giftcargado = getLayoutInflater().inflate(R.layout.giftcargado,
+                        parent, false);
+            }
+            // Find the car to work with.
+            Gift currentgift = gifts.get(position);
+
+            }
+        // Fill the view
+        ImageView imageView =
+                (ImageView)itemView.findViewById(R.id.item_icon);
+         imageView.setImageResource(currentCar.getIconID());
+        // Fabricante:
+        TextView makeText = (TextView)
+                itemView.findViewById(R.id.item_txtMake);
+makeText.setText(currentCar.getMake());
+
+        return giftcargado;
+    }
 }
+J

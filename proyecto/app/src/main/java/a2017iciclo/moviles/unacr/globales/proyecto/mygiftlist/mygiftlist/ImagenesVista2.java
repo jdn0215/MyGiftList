d93e0c5a16aad4b5fe014e8e61cd7314e3c8e20d
luisrodriguezchaves1 <luigi05-3@hotmail.com>
2017-05-25 22:40:32 -0600

    corrigiendo la agregacion anterior

--- proyecto/app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/ImagenesVista2.java
+++ proyecto/app/src/main/java/a2017iciclo/moviles/unacr/globales/proyecto/mygiftlist/mygiftlist/ImagenesVista2.java
@@ -1,6 +1,6 @@
 package a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist;
 
-<<<<<<< HEAD
+
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -18,11 +18,9 @@ import java.util.List;
 import a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db.DaoMaster;
 import a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db.DaoSession;
 import a2017iciclo.moviles.unacr.globales.proyecto.mygiftlist.mygiftlist.db.GiftDBDao;
-=======
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
->>>>>>> e69b04715a089a851e24575b38e79a7a5db12436
 
 
 public class ImagenesVista2 extends BaseDatos{
@@ -33,11 +31,9 @@ public class ImagenesVista2 extends BaseDatos{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imagenes_vista2);
         folderactual=super.getIntent().getStringExtra("folderactual");
-<<<<<<< HEAD
+
         cargarGifts();
-=======
-        super.sizeLista();
->>>>>>> e69b04715a089a851e24575b38e79a7a5db12436
+
     }
 
     @Override
